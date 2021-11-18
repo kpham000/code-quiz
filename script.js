65cc9d98cbadfccf6f9ac7bb67e24ba106c4ddfa
kpham000 <90490117+kpham000@users.noreply.github.com>
2021-11-18 01:19:51 -0800

    timer countdown

--- script.js
+++ script.js
@@ -37,116 +37,45 @@ function updateCountdown() {
  
 
 // quiz part
-(function(){
-    function buildQuiz(){
-      // variable to store the HTML output
-      var output = [];
-  
-      // for each question...
-      myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-  
-          // variable to store the list of possible answers
-          var answers = [];
-  
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
-  
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-            );
-          }
-  
-          // add this question and its answers to the output
-          output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>`
-          );
-        }
-      );
-  
-      // finally combine our output list into one string of HTML and put it on the page
-      quizContainer.innerHTML = output.join('');
-    }
-  
-    function showResults(){
-  
-      
-      var answerContainers = quizContainer.querySelectorAll('.answers');
-  
-      // keep track answers
-      let numCorrect = 0;
-  
-      
-      myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-       
-        var answerContainer = answerContainers[questionNumber];
-        var selector = `input[name=question${questionNumber}]:checked`;
-        var userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-        // if answer is correct
-        if(userAnswer === currentQuestion.correctAnswer){
-          
-          numCorrect++;
-  
-          
-          answerContainers[questionNumber].style.color = 'lightgreen';
-        }
-        else{
-          
-          answerContainers[questionNumber].style.color = 'red';
-        }
-      });
-  
-      // show number of correct answers 
-      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
-  
-    var quizContainer = document.getElementById('quiz');
-    var resultsContainer = document.getElementById('results');
-    var submitButton = document.getElementById('submit');
-    var myQuestions = [
-      {
-        question: "Inside which HTML element do we put the javascript?",
-        answers: {
-          a: "<scripting>",
-          b: "<javascript>",
-          c: "<script>"
-        },
-        correctAnswer: "c"
-        },
-        {
-        question: "Where is the correct place to insert a JavaScript?",
-        answers: {
-        a: "<head> section",
-        b: "<head> and <body> section",
-        c: "<body> section"
-        },
-        correctAnswer: "c"
-        },
-        {
-        question: "How do you create a function in JavaScript?",
-        answers: {
-        a: "function(myFunction)",
-        b: "function = myFunction()",
-        c: "function:myFunction()",
-        d: "function myFunction()"
-        },
-        correctAnswer: "d"
-        }
-  ];
 
-    buildQuiz();
-  
-    // Event 
-    submitButton.addEventListener('click', showResults);
-  })();
+const questions = [
+  {
+    question: "Inside which HTML element do we put the javascript?",
+    answers: ["<scripting>", "<javascript>", "<script>"],
+    correctAnswer: "<script>"
+  },
+
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    answers: ["<head> section", "<head> and <body> section", "<body> section"],
+    correctAnswer: "<body> section"
+  },
+
+  {
+    question: "How do you create a function in JavaScript?",
+    answers: ["function(myFunction)", "function = myFunction()", "function:myFunction()","function myFunction()"],
+    correctAnswer: "function myFunction()"
+  },
+];
+
+// timer countdown
+let timerSecs = 0;
+let currentQuestion = 0
+let score = 0;
+let scoreArray = [];
+let timerInterval = false;
+
+timerSecs = 60;
+timerDisplay.textContent = timerSecs;
+
+countdown();
+
+nextQuestion();
+
+startButton.style.display = 'none';
+}
+
+
 
 // THEN I am presented with another question
 // WHEN I answer a question incorrectly
